ada89a9c637162a32f1784a62c28961828227406
Calvin Joy Tarigan <joycalvin1604@gmail.com>
2026-07-03 07:53:38 +0700

    feat: set up React root and i18n initialization

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { Suspense } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./i18n";
 import App from "./App.jsx";
+import ArenaView from "./components/ArenaView.jsx";
 import "./styles.css";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Suspense fallback={<div style={{ padding: 40, textAlign: "center" }}>Loading...</div>}>
+        <Routes>
+          <Route path="/arena" element={<ArenaView />} />
+          <Route path="*" element={<App />} />
+        </Routes>
+      </Suspense>
+    </BrowserRouter>
   </React.StrictMode>,
 );
